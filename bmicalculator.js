79beddfd090eb9edaa47a4734685978bf5abc377
cheyyziii <nanlabicheska916@gmail.com>
2025-12-12 08:57:39 +0800

    Co-authored-by: jayjayobiasada <jayobiasda@users.noreply.github.com> Co-authored-by: SirSeraspe <SirSeraspeCZSHS@users.noreply.github.com>

---
+++ bmicalculator.js
@@ -0,0 +1,3 @@
+const weatherInput = document.getElementById('weatherInput');
+const weatherBtn = document.getElementById('weatherBtn');
+const weatherResult = document.getElementById('weatherResult');
